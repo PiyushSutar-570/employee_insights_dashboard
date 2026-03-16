ee34e50685a5330e8a3383e7ca54bc28a144acb3
PiyushSutar-570 <piyushsutar570@gmail.com>
2026-03-16 20:10:01 +0530

    added routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
+import List from './pages/List'
+import Login from './pages/Login'
+import Details from './pages/Details'
+import Analytics from './pages/Analytics'
 const App = () => {
   return (
-    <div className='text-8xl'>
-      hello
-    </div>
+    <Routes>
+      <Route path='/' element={<Login/>} />
+      <Route path='/' element={<List/>} />
+      <Route path='/details/:id' element={<Details/>}/>
+      <Route path='/analytics' element={<Analytics/>} />
+    </Routes>
   )
 }
 
